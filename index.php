1e4c4d749bcf4494ca1e6c0f80edcf2aa573772b
Moroz Michael <bigmichael1922@gmail.com>
2023-09-16 22:10:01 +0600

    feauture addded

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-print "Hello world!";
+
+function summ($a, $b){
+    $summ = $a+$b;
+    return $summ;
+}
+
+
+
+$person = [
+    "id" => 234,
+    "name" => "Misha",
+    'age' => 25,
+    'hobbies' => [
+        "music",
+        "programming",
+        "sport"
+    ],
+];
+
+foreach ($person['hobbies'] as $hobby){
+    print($hobby."\n");
+}
+
+
